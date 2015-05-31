e8f7e39aa450da5ede7356d3b9049ef019644ad5
danturne <danturne@yahoo.com>
2015-05-30 14:32:55 -1000

    completed exercise

--- vault.js
+++ vault.js
@@ -1,4 +1,34 @@
 'use strict';
 module.exports = function() {
-  
+
+  var keyValues = [];
+
+  var _setValue = function(key,value) {
+
+    if(_getValue(key) !== undefined) {
+
+      keyValues.push([key,value]);
+    }
+
+  };
+
+  var _getValue = function(key) {
+
+    for(var i = 0; i < keyValues.length; i++) {
+
+      if(keyValues[i][0] === key) {
+
+        return keyValues[i][1];
+      }
+    }
+
+    return null;
+  };
+
+  return {
+
+    setValue: _setValue,
+    getValue: _getValue
+  };
+
 };
